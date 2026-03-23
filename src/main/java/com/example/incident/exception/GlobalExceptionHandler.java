8e5cdbd6ebe34e2cca39a60ac456d65d87c21925
Irina Gor <vipgorina@mail.ru>
2026-03-23 14:38:14 +0300

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/example/incident/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.incident.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
